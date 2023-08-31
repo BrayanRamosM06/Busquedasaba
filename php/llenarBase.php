fcd7ccc8c45b84fdbec98c63cd995a4d12c0f3cd
Brayan Ramos <baryan.r94@gmail.com>
2023-08-30 21:07:11 -0600

    inserto cambios para el efecto de las burbujas

--- php/llenarBase.php
+++ php/llenarBase.php
@@ -13,25 +13,14 @@ foreach ($patch as $lineass) {
     $sku = ($lineas[0]);
     $sap = ($lineas[1]);
     $descripcion = ($lineas[2]);
-    $barra   = ($lineas[3]);
+    $barra = ($lineas[3]);
 
-    $sql= "SELECT *FROM codigosaba  WHERE sku=?";
-    $stmt = mysqli_prepare($db, $sql);
-    mysqli_stmt_bind_param($stmt,'s', $sku);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-    
-    if(mysqli_num_rows($result) >0){
-        
-        
-        $stmt =mysqli_prepare($db,"UPDATE  codigosaba SET sku = ?, sap = ? ,descripcion = ? ,barra = ?  WHERE sku = $sku");
-        mysqli_stmt_bind_param($stmt,'ssss', $sku, $sap, $descripcion, $barra);
-        mysqli_stmt_execute($stmt);
-        $stmt->close();
-        // $stmt = $db-> mysqli_prepare($update);
-        
-    }else{
-        
+   // $sql= "SELECT *FROM codigosaba  WHERE sku=?";
+    //$stmt = mysqli_prepare($db, $sql);
+    //mysqli_stmt_bind_param($stmt,'s', $sku);
+    //mysqli_stmt_execute($stmt);
+    //$result = mysqli_stmt_get_result($stmt);
+
         $query ="INSERT INTO codigosaba (sku, sap, descripcion, barra) VALUES (?,?,?,?) ";
         $stmt = mysqli_prepare($db,$query);
         mysqli_stmt_bind_param($stmt, 'ssss', $sku,$sap, $descripcion, $barra);
@@ -41,9 +30,33 @@ foreach ($patch as $lineass) {
             echo("Registrado correctamente");    
             
         }else{
-            echo "Error al insertar". $insert."<br>". mysqli_error($db);
+            echo "Error al insertar"."<br>". mysqli_error($db);
         }
-    }    
+
+    
+   // if(mysqli_num_rows($result) >0){
+        
+        
+      //  $stmt =mysqli_prepare($db,"UPDATE  codigosaba SET sku = ?, sap = ? ,descripcion = ? ,barra = ?  WHERE sku = $sku");
+       // mysqli_stmt_bind_param($stmt,'ssss', $sku, $sap, $descripcion, $barra);
+        //mysqli_stmt_execute($stmt);
+        //$stmt->close();
+        // $stmt = $db-> mysqli_prepare($update);
+        
+   // }else{
+        
+     //   $query ="INSERT INTO codigosaba (sku, sap, descripcion, barra) VALUES (?,?,?,?) ";
+       // $stmt = mysqli_prepare($db,$query);
+       // mysqli_stmt_bind_param($stmt, 'ssss', $sku,$sap, $descripcion, $barra);
+        //mysqli_stmt_execute($stmt);
+
+      //  if (mysqli_query($db,$query)) {
+      //      echo("Registrado correctamente");    
+            
+      //  }else{
+      //      echo "Error al insertar". $insert."<br>". mysqli_error($db);
+       // }
+   // }    
   
 }
 mysqli_close($db);
